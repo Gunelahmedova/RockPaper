21a1104d1fe08db831278d5f0dd10e0ebdf20e7a
gunelahmedova <gunelcavadqizi5@gmail.com>
2023-08-29 10:46:10 +0400

    create index.html

---
+++ dom.js
@@ -0,0 +1,7 @@
+
+const playerFirstTitle = document.querySelector("#playerFirstTitle"),
+ playerFirstScore = document.querySelector("#playerFirstScore"),
+ playerFirstHand = document.querySelector("#playerFirstHand"),
+ playerSecondTitle = document.querySelector("#playerSecondTitle"),
+ playerSecondScore = document.querySelector("#playerSecondScore"),
+ playerSecondHand = document.querySelector("#playerSecondHand");
